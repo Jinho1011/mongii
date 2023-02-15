ccb419746f4bd5470c8315176fa7d12412c04678
Jeon <gino9940@gmail.com>
2023-02-15 11:07:04 +0900

    build: add scss setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("node:path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/shared/styles")],
+    // prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
