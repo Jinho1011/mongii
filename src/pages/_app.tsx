65bd7c4e465997721e8db6a92a19f4ca2bc88d4e
Jeon <gino9940@gmail.com>
2023-02-22 04:50:21 +0900

    build: add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from "next/app";
 import "@/shared/styles/reset.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
